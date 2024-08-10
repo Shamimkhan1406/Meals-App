193cd04236574392d6b3eb0e744a235ea9df7188
Shamimkhan1406 <shamim4khan0@gmail.com>
2024-08-11 02:14:42 +0530

    improving meald detail Screen

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -11,14 +11,46 @@ class DetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: 
-        Image.network(meal.imageUrl),
-      // Column(
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   children: [
-      //     Text(meal.title,style: const TextStyle(fontSize: 30),),
-      //   ],
-      // ),
+      body:
+      SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 16,),
+            Text('Ingredient',style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+            ),),
+            const SizedBox(height: 16,),
+            for(final ingredient in meal.ingredients)
+              Text(ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+            const SizedBox(height: 16,),
+            Text('Steps',style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+            ),),
+            const SizedBox(height: 16,),
+            for(final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 16),
+                child: Text(step,
+                textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
